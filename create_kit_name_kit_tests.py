13a9c1b4b4cd6fb4ecd242256501c32d6eed8fad
HerdisLund <sa.anymed@gmail.com>
2024-11-22 22:37:29 +0300

    Added the positive_assert function. Reworked all positive tests and the 11th negative test. Added the function of getting the authenticator for the user. Added functionality that allows you to take the received authenticator and put it in the function of creating a product set. Removed unnecessary links from the configuration file. Updated the header in the data file

--- create_kit_name_kit_tests.py
+++ create_kit_name_kit_tests.py
@@ -1,96 +1,73 @@
 import sender_stand_request # Импорт модуля с запросами
 import data # Импорт модуля с данными
 
-# Эта функция меняет значение в параметре name
+# Функция замены значения в параметре name
 def get_kit_body(kit_name):
     current_kit_body = data.kit_body.copy() # Копирование словаря с телом запроса из файла data, чтобы не потерять данные в исходном словаре
     current_kit_body["name"] = kit_name # Изменение значения в поле name
     return current_kit_body # Возвращается новый словарь с нужным значением name
 
+# Функция для позитивной проверки
+def positive_assert(kit_name):
+    kit_body_positive = get_kit_body(kit_name)  # В переменную kit_body сохраняется обновлённое тело запроса
+    kit_response_positive = sender_stand_request.create_kit_name(kit_body_positive)
+
+    assert kit_response_positive.json()["name"] == kit_name # Проверка, что имя набора продуктов соответствует ожидаемому
+    assert kit_response_positive.status_code == 201 # Проверка, что код ответа равен 201
+
+# Функция для негативной проверки
+def negative_assert(kit_name):
+    kit_body_negative = get_kit_body(kit_name) # В переменную kit_body_negative сохраняется обновлённое тело запроса
+    kit_response_negative = sender_stand_request.create_kit_name(kit_body_negative) # В переменную kit_response_negative сохраняется результат запроса
+
+    assert kit_response_negative.status_code == 400 # Проверка, что код ответа равен 400
+    assert kit_response_negative.json()["code"] == 400 # Проверка, что в теле ответа атрибут "code" равен 400
+
 # Тест 1. Успешное создание набора продуктов для существующего пользователя [Параметр name = 1 символ]
 def test_create_kit_1_letter_in_kit_name_get_success_response():
-    kit_body = get_kit_body("a") # В переменную kit_body сохраняется обновленное тело запроса с именем "а"
-    kit_response = sender_stand_request.create_kit_name(kit_body) # В переменную kit_response сохраняется результат запроса на создание пользователя
-
-    assert kit_response.status_code == 201 # Проверяется, что код ответа равен 201
-    assert kit_response.json()["name"] == "a" # Проверяется, что заполнено name со значением "а"
+    positive_assert("a")
 
 # Тест 2. Успешное создание набора продуктов для существующего пользователя [Параметр name = 511 символов]
 def test_create_kit_511_letters_in_kit_name_get_success_response():
-    kit_body = get_kit_body("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC") # В переменную kit_body сохраняется обновленное тело запроса с именем из 511 символов
-    kit_response = sender_stand_request.create_kit_name(kit_body) # В переменную kit_response сохраняется результат запроса на создание пользователя
-
-    assert kit_response.status_code == 201 # Проверяется, что код ответа равен 201
-    assert kit_response.json()["name"] == "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC" # Проверяется, что заполнено name со значением из 511 символов
-
-# Эта функция для негативной проверки
-def negative_assert_symbol(kit_name):
-    kit_body = get_kit_body(kit_name) # В переменную kit_body сохраняется обновлённое тело запроса
-    response = sender_stand_request.create_kit_name(kit_body) # В переменную response сохраняется результат запроса
-
-    assert response.status_code == 400 # Проверка, что код ответа равен 400
-    assert response.json()["code"] == 400 # Проверка, что в теле ответа атрибут "code" равен 400
+    positive_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
 
 # Тест 3. Ошибка при создании набора продуктов для существующего пользователя [Параметр name = пустой ввод]
 def test_create_kit_empty_input_in_kit_name_get_error_response():
-    negative_assert_symbol("")
+    negative_assert("")
 
 # Тест 4. Ошибка при создании набора продуктов для существующего пользователя [Параметр name = 512 символов]
 def test_create_kit_512_letter_in_kit_name_get_error_response():
-        negative_assert_symbol("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
+        negative_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
 
 # Тест 5. Успешное создание набора продуктов для существующего пользователя [Параметр name англ буквы = "QWErty"]
 def test_create_kit_english_letter_in_kit_name_get_success_response():
-    kit_body = get_kit_body("QWErty")  # В переменную kit_body сохраняется обновленное тело запроса с именем "QWErty"
-    kit_response = sender_stand_request.create_kit_name(
-        kit_body)  # В переменную kit_response сохраняется результат запроса на создание пользователя
-
-    assert kit_response.status_code == 201  # Проверяется, что код ответа равен 201
-    assert kit_response.json()["name"] == "QWErty"  # Проверяется, что заполнено name со значением "QWErty"
+    positive_assert("QWErty")
 
 # Тест 6. Успешное создание набора продуктов для существующего пользователя [Параметр name русс буквы = "Мария"]
 def test_create_kit_russian_letter_in_kit_name_get_success_response():
-    kit_body = get_kit_body("Мария")  # В переменную kit_body сохраняется обновленное тело запроса с именем "Мария"
-    kit_response = sender_stand_request.create_kit_name(
-        kit_body)  # В переменную kit_response сохраняется результат запроса на создание пользователя
-
-    assert kit_response.status_code == 201  # Проверяется, что код ответа равен 201
-    assert kit_response.json()["name"] == "Мария"  # Проверяется, что заполнено name со значением "Мария"
+    positive_assert("Мария")
 
 # Тест 7. Успешное создание набора продуктов для существующего пользователя [Параметр name спец символы  = ""№%@",]
 def test_create_kit_symbol_in_kit_name_get_success_response():
-    kit_body = get_kit_body('"№%@",')  # В переменную kit_body сохраняется обновленное тело запроса с именем "№%@",
-    kit_response = sender_stand_request.create_kit_name(
-        kit_body)  # В переменную kit_response сохраняется результат запроса на создание пользователя
-
-    assert kit_response.status_code == 201  # Проверяется, что код ответа равен 201
-    assert kit_response.json()["name"] == '"№%@",' # Проверяется, что заполнено name со значением "№%@",
+    positive_assert('"№%@",')
 
 # Тест 8. Успешное создание набора продуктов для существующего пользователя [Параметр name содержит пробелы = " Человек и КО "]
 def test_create_kit_name_with_spaces_get_success_response():
-    kit_body = get_kit_body(" Человек и КО ")  # В переменную kit_body сохраняется обновленное тело запроса с именем " Человек и КО "
-    kit_response = sender_stand_request.create_kit_name(
-        kit_body)  # В переменную kit_response сохраняется результат запроса на создание пользователя
-
-    assert kit_response.status_code == 201  # Проверяется, что код ответа равен 201
-    assert kit_response.json()["name"] == " Человек и КО "  # Проверяется, что заполнено name со значением " Человек и КО "
+    positive_assert(" Человек и КО ")
 
 # Тест 9. Успешное создание набора продуктов для существующего пользователя [Параметр name цифры строкой = 123]
 def test_create_kit_str_numbers_in_kit_name_get_success_response():
-    kit_body = get_kit_body("123")  # В переменную kit_body сохраняется обновленное тело запроса с именем 123
-    kit_response = sender_stand_request.create_kit_name(
-        kit_body)  # В переменную kit_response сохраняется результат запроса на создание пользователя
-
-    assert kit_response.status_code == 201  # Проверяется, что код ответа равен 201
-    assert kit_response.json()["name"] == "123"  # Проверяется, что заполнено name со значением 123
+    positive_assert("123")
 
 # Тест 10. Ошибка при создании набора продуктов для существующего пользователя [Параметр name не передан]
 def test_create_kit_dont_get_kit_name_get_error_response():
-        negative_assert_symbol()
+    current_kit_body_without_name = data.kit_body.copy() # Копирую словарь с телом kit_body
+    current_kit_body_without_name.pop("name") # Удаляю параметр name
+    negative_assert(current_kit_body_without_name) # Проверяю ответ
 
 # Тест 11. Ошибка при создании набора продуктов для существующего пользователя [Параметр name целое число]
 def test_create_kit_int_numbers_in_kit_name_get_error_response():
-    negative_assert_symbol(123)
+    negative_assert(123)
 
 
 
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,17 +7,16 @@ def post_new_user(body):
        return requests.post(configuration.BASE_URL + configuration.CREATE_USER_PATH,
                          json=body,
                          headers=data.headers)
-response = post_new_user(data.user_body) # Выполнить функцию с использованием данных user_body
 
-print(response.status_code) # Вывести код статуса
-print(response.json()) # Вывести полученный ответ
+# Функция получения аутентификатора для пользователя
+def get_new_user_token():
+    response = post_new_user(data.user_body)
+    return response.json().get("authToken") # Извлекаем значение по ключу "authToken", выданный сервером после успешной регистрации пользователя
 
-# Функция создания нового набора продуктов для определенного пользователя
+# Функция создания нового набора продуктов для пользователя с полученным аутентификатором
 def create_kit_name(body):
+    headers_with_token = data.headers.copy()
+    headers_with_token["Authorization"] = "Bearer " + get_new_user_token() # В копию заголовков добавляется новый ключ Authorization. В значение добавляется строка "Bearer " + добавляем сам токен, который был получен из функции get_new_user_token()
     return requests.post(configuration.BASE_URL + configuration.CREATE_KITS_PATH,
                          json=body,
-                         headers=data.headers_for_create_kit)
-response = create_kit_name(data.kit_body) # Выполнить функцию с использованием данных kit_body
-
-print(response.status_code) # Вывести код статуса
-print(response.json()) # Вывести полученный ответ
+                         headers=headers_with_token)
